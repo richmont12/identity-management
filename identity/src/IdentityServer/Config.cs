4efbd2a12983d1280fc30ded4a94bf375058404c
Richard Heininger <rhe@indeca.de>
2023-05-03 16:43:16 +0200

    feat: added oauth auth code login

--- identity/src/IdentityServer/Config.cs
+++ identity/src/IdentityServer/Config.cs
@@ -1,38 +1,64 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace IdentityServer;
 
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        { 
-            new IdentityResources.OpenId()
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
         };
+
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
-            { 
-                new ApiScope(name: "dataapi", displayName: "Data Api")
-            };
+        {
+            new ApiScope(name: "dataapi", displayName: "Data Api")
+        };
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
-            {
-        new Client
+        new Client[]
         {
-            ClientId = "webclient",
-
-            // no interactive user, use the clientid/secret for authentication
-            AllowedGrantTypes = GrantTypes.ClientCredentials,
-
-            // secret for authentication
-            ClientSecrets =
+            new Client
             {
-                new Secret("secret".Sha256())
-            },
+                ClientId = "machineclient",
 
-            // scopes that client has access to
-            AllowedScopes = { "dataapi" }
-        } };
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "dataapi" }
+            },
+            new Client
+            {
+                ClientId = "webclient",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                // where to redirect to after login
+                RedirectUris = { "http://localhost:3000/api/auth/callback/identity-provider" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris =
+                {
+                    "http://localhost:3000"
+                },
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                }
+            }
+        };
 }

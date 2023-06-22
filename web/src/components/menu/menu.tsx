14c8ce3993f2f753184769e7e8d0596c1c5b198e
Richard Heininger <rhe@indeca.de>
2023-06-22 18:30:33 +0200

    refactor: seperated data 1 and 2

--- web/src/components/menu/menu.tsx
+++ web/src/components/menu/menu.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import Head from 'next/head'
+import { useSession } from "next-auth/react";
 
 export const Menu: React.FunctionComponent = () => {
+    const { data: session } = useSession();
+
+    if(session) {
+        return (
+            <div className="menu">
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="data1">data1</a>
+                    </li>
+                    <li>
+                        <a href="data2">data2</a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+
+    
     return (
         <div className="menu">
             <ul>
-                <li>
-                    <a href="welcome">Welcome</a>
-                </li>
                 <li>
                     <a href="/">Home</a>
                 </li>
--- web/src/pages/data1/index.tsx
+++ web/src/pages/data1/index.tsx
@@ -4,7 +4,6 @@ import React, { useEffect, useState } from "react";
 
 export default function Welcome() {
     const [mighty, setMighty] = useState([]);
-    const [mighty2, setMighty2] = useState([]);
     useEffect(() => {
         fetch('http://localhost:3000/api/mightydata')
             .then((response) => response.json())
@@ -15,25 +14,13 @@ export default function Welcome() {
                 setMighty("you are not authorized for mighty1");
                 console.log(err.message);
             });
-
-        fetch('http://localhost:3000/api/mightydata2')
-            .then((response) => response.json())
-            .then((data) => {
-                setMighty(data.mightyData2);
-            })
-            .catch((err) => {
-                setMighty("you are not authorized for mighty2");
-                console.log(err.message);
-            });
     }, []);
     return (
         <Layout>
 
-            Welcome to Identity Management Web
-
+            Data 1
+            <br/>
             <p>{ mighty }</p>
-            <hr></hr>
-            <p>{ mighty2 }</p>
 
         </Layout>
     )
